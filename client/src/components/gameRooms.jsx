6e58a4b35d605d5ec2bdfb240b245e9222adaed6
ryanWang2018 <ryanwei.wang@mail.utoronto.ca>
2019-03-27 21:12:50 -0400

    fewfew

--- client/src/components/gameRooms.jsx
+++ client/src/components/gameRooms.jsx
@@ -204,10 +204,7 @@ class GameRooms extends Component {
             &raquo;
           </button>
         </div>
-        <button
-          onClick={() => this.handleAddRoom}
-          className="btn btn-lg btn-danger"
-        >
+        <button onClick={this.handleAddRoom} className="btn btn-lg btn-danger">
           Create Room
         </button>
       </div>
